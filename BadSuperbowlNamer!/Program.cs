bffb541be8ced6ca50621078054aa2508205092e
unknown <c.chase.hamby@gmail.com>
2019-04-02 21:00:13 -0500

    test

---
+++ BadSuperbowlNamer!/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BadSuperbowlNamer_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var converter = new RomanNumeralConverter();
+
+            Console.WriteLine("Give me a number");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            var result = converter.ConvertStuff(input);
+            Console.WriteLine($"{input} is converted to the roman numeral {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BadSuperbowlNamer!/RomanNumeralConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadSuperbowlNamer_
+{
+    public class RomanNumeralConverter
+    {
+        public string ConvertStuff(int number)
+        {
+            if (number >= 9) return "IX";
+            if (number >= 5) return "V";
+            if (number >= 4) return "IV";
+            if (number >= 1) return "I";
+
+            var convertedNumber = number.ToString();
+
+            return convertedNumber;
+        }
+    }
+}
