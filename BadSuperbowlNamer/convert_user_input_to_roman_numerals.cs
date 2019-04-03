a1451b8194ef620c900ac798f1724fe9dfef8c18
unknown <c.chase.hamby@gmail.com>
2019-04-02 20:55:51 -0500

    added more tests

--- BadSuperbowlNamer/convert_user_input_to_roman_numerals.cs
+++ BadSuperbowlNamer/convert_user_input_to_roman_numerals.cs
@@ -22,12 +22,42 @@ namespace BadSuperbowlNamer.Tests
         }
 
         [Fact]
+        public void number_4_should_be_roman_numberal_IV()
+        {
+            // Arrange
+            var converter = new RomanNumeralConverter();
+            var input = 4;
+            var expectedResult = "IV";
+
+            // Act
+            var actualResult = converter.ConvertStuff(input);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
         public void number_5_should_be_roman_numberal_V()
         {
             // Arrange
             var converter = new RomanNumeralConverter();
             var input = 5;
             var expectedResult = "V";
+
+            // Act
+            var actualResult = converter.ConvertStuff(input);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void number_9_should_be_roman_numberal_IX()
+        {
+            // Arrange
+            var converter = new RomanNumeralConverter();
+            var input = 9;
+            var expectedResult = "IX";
 
             // Act
             var actualResult = converter.ConvertStuff(input);
